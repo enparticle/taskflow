0b91fbb2f1557a7d84b1e8ce092afc4f8166e18f
Sunghoon Kim <sunghoonkim1991@gmail.com>
2026-05-18 15:10:06 +0900

    increase body size limit to 25mb

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,9 @@
-@"
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
-  api: {
-    bodyParser: {
-      sizeLimit: '25mb',
-    },
-    responseLimit: '25mb',
-  },
   experimental: {
     serverActions: {
-      bodySizeLimit: '25mb',
+      bodySizeLimit: "25mb",
     },
   },
 };
 export default nextConfig;
-"@ | Set-Content "next.config.ts"
